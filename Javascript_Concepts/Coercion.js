bc83d3fb1dfae4f037a97290499f70f09f2d5f28
Pooja Tikhe <47672660+tikhepooja11@users.noreply.github.com>
2024-01-17 20:57:54 +0530

    Update Coercion.js

--- Javascript_Concepts/Coercion.js
+++ Javascript_Concepts/Coercion.js
@@ -11,3 +11,19 @@ example -
 1 === true; // false, because the types are different (number vs. boolean)
 
 In general, it's a good practice to use === for equality checks in JavaScript to avoid unintended type coercion and make your code more predictable.
+-----------------------------------------------------------------------------------------------------
+String coercion - when string + number/boolean/array/object => Others are converted to string 
+
+Number coercion - When maths operators (- or * or / or % ) then always to numbers 
+    2. True + 1 = 2
+
+Boolean Coercion - 
+    When boolean + number = always number 
+        1. True + 3 = 4 and True - 1 = 0 
+    
+    When boolean + string = always string 
+        1. True + '1' = true1 
+        2. True - '1' = 0 (true = 1 and false = 0) 
+    
+    Boolean + boolean always number 
+        1. TRUE + TRUE = 2 
